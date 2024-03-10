2dc0fffd72c23163b32793d9b34010e1ac2f5e58
yshelikh <yshelikh@gmail.com>
2024-03-10 21:20:01 +0300

    IDE

---
+++ game.py
@@ -0,0 +1,12 @@
+import numpy as np
+import numpy as np
+def random_predict(number:int=1) -> int:
+    """_summary_
+
+    Args:
+        number (int, optional): _description_. Defaults to 1.
+
+    Returns:
+        int: _description_
+    """    ''''''
+    
